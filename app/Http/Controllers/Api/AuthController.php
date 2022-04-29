dbd354d604fabc7ecb018b4d7e8a6cbe7c217a64
SidneyHodieb <shodieb@gmail.com>
2022-04-29 21:20:57 +0200

    Transfert backend to new PC

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\User;
+
+class AuthController extends Controller
+{
+
+    public function register(Request $request)
+    {
+        $user = new User();
+        $user->fisrt_name = $request->firstname;
+        $user->last_name = $request->lastname;
+        $user->email = $request->email;
+        $user->password = bcrypt($request->password);
+        $user->save();
+        return response()->json(['data' => $user]);
+    }
+
+    public function login()
+    {
+        $credentials = request(['email', 'password']);
+
+        if (!$token = auth('api')->attempt($credentials)) {
+            return response()->json(['error' => 'Unauthorized'], 401);
+        }
+        return $this->getToken($token);
+    }
+
+    public function getUser()
+    {
+        return response()->json(auth('api')->user());
+    }
+
+    public function logout()
+    {
+        auth('api')->logout();
+        return response()->json(['message' => 'Logged out successfully']);
+    }
+
+    public function refreshToken()
+    {
+        return $this->getToken(auth('api')->refresh());
+    }
+
+    protected function getToken($token)
+    {
+        return response()->json([
+            'access_token' => $token,
+            'token_type' => 'bearer',
+            'expires_in' => auth('api')->factory()->getTTL() * 60
+        ]);
+    }
+}
